8d900bcc0e06a799f5e056a5145544baee03ba61
saif <saifullahmohammad512008@gmail.com>
2025-12-26 14:31:49 +0530

    server setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import http from 'http';
+import app from './src/app.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
